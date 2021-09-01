382df57f7e5780b3f492c298efb53845aefa9b56
Diogo Deconto <dideconto@gmail.com>
2021-09-01 12:18:37 -0300

    CRUD de Produtos com códigos HTTP na resposta

--- API/Controllers/ProdutoController.cs
+++ API/Controllers/ProdutoController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using API.Data;
@@ -19,16 +20,57 @@ namespace API.Controllers
         //POST: /api/produto/create
         [Route("create")]
         [HttpPost]
-        public Produto Create(Produto produto)
+        public IActionResult Create([FromBody] Produto produto)
         {
             _context.Produtos.Add(produto);
             _context.SaveChanges();
-            return produto;
+            return Created("", produto);
         }
 
         //GET: /api/produto/list
         [Route("list")]
         [HttpGet]
-        public List<Produto> List() => _context.Produtos.ToList();
+        public IActionResult List() => Ok(_context.Produtos.ToList());
+
+        //GET: api/produto/getbyid/1
+        [HttpGet]
+        [Route("getbyid/{id}")]
+        public IActionResult GetById([FromRoute] int id)
+        {
+            //Buscar um objeto pela chave primária
+            Produto produto = _context.Produtos.Find(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return Ok(produto);
+        }
+
+        //GET: api/produto/delete/Bolacha
+        [HttpDelete]
+        [Route("delete/{name}")]
+        public IActionResult Delete(string name)
+        {
+            //Where -> Expressão lambda
+            //Buscar um objeto pelo nome
+            Produto produto = _context.Produtos.FirstOrDefault(produto => produto.Nome == name);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok(_context.Produtos.ToList());
+        }
+
+        //PUT: /api/produto/create
+        [Route("update")]
+        [HttpPut]
+        public IActionResult Update([FromBody] Produto produto)
+        {
+            _context.Produtos.Update(produto);
+            _context.SaveChanges();
+            return Ok(produto);
+        }
     }
 }
